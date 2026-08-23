b78484241320ace8c488576f2e518ef0032caf07
Aya Shehab <47066398+AyaShehab@users.noreply.github.com>
2026-08-23 21:23:07 +0300

    Update AuthContext.jsx

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,21 +1,29 @@
-import { createContext, useState } from 'react';
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
-export const AuthContext = createContext();
-
-export default function AuthContextProvider({ children }) {
-  const [userToken, setuserToken] = useState(localStorage.getItem('token') || null);
-  const [userData, setuserData] = useState(null);
-
- 
-  function logOut() {
-    localStorage.removeItem('token');
-    setuserToken(null);
-    setuserData(null);
-  }
-
-  return (
-    <AuthContext.Provider value={{ userToken, setuserToken, userData, setuserData, logOut }}>
-      {children}
-    </AuthContext.Provider>
-  );
+export let AuthContext =createContext();
+export function AuthContextProvider({children}){
+    const [userToken, setuserToken] = useState(null)
+    const [userData, setuserData] = useState(null)
+   async function getUserData(){
+     let {data}= await   axios.get('https://route-posts.routemisr.com/users/profile-data',{
+            headers:{
+                Authorization:`Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        setuserData(data.data.user)
+    }
+   
+    useEffect(()=>{
+        if(localStorage.getItem('token')!== null){
+            setuserToken(localStorage.getItem('token'))
+            
+            getUserData()
+        }
+    },[])
+return(
+<AuthContext.Provider value={{userToken , setuserToken,userData}}>
+{children}
+</AuthContext.Provider>
+);
 }

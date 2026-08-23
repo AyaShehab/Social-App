d18d89fee72e2cb1a821746a0ff9372caacc823c
Aya Shehab <47066398+AyaShehab@users.noreply.github.com>
2026-08-23 20:29:00 +0300

    Update AuthContext.jsx

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,18 +1,20 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
   const [userToken, setuserToken] = useState(localStorage.getItem('token') || null);
+  const [userData, setuserData] = useState(null);
 
  
   function logOut() {
     localStorage.removeItem('token');
-    setuserToken(null); 
+    setuserToken(null);
+    setuserData(null);
   }
 
   return (
-    <AuthContext.Provider value={{ userToken, setuserToken, logOut }}>
+    <AuthContext.Provider value={{ userToken, setuserToken, userData, setuserData, logOut }}>
       {children}
     </AuthContext.Provider>
   );
